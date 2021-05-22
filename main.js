cdb677c65772c3612433b10a08e5f2cdf02169d6
Jemma Holmes <jemmaholmes@Jemmas-MacBook-Pro.local>
2021-05-22 20:36:50 +0100

    Eggs added and CSS adjusted

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+const eggNormal = document.querySelector('#egg-normal')
+const eggSlightCrack = document.querySelector('#egg-slightly-cracked')
+const eggCracked = document.querySelector('#egg-cracked')
+
+
+// for(i = 0; i < 5; i++) {
+//   eggNormal.addEventListener(('click'), () => {
+//     firstCrack = () => {
+//       eggNormal.style.opacity = "0"
+//       return eggSlightCrack.style.opacity = "100%"
+//     }
+//   })  
+// };
+// console.log(firstCrack());
+
+
+
 const originalImage = document.querySelector('#dave-normal')
+const flapImage = document.querySelector('#daveflap')
 
 const feed = document.querySelector('#feedhim');
 const fedImage = document.querySelector('#davefed');
@@ -8,6 +26,13 @@ const loveImage = document.querySelector('#daveloved');
 
 const fire = document.querySelector('#firehim');
 const fireImage = document.querySelector('#davefire');
+
+// daveFlap = () => {
+//   originalImage.style.opacity = "0"
+//   return flapImage.style.opacity = "100"
+// };
+
+// setInterval(daveFlap(), 3*1000);
 
 daveFed = () => {
   originalImage.style.opacity = "0"
@@ -19,20 +44,21 @@ daveLoved = () => {
   return loveImage.style.opacity = "100%"
 };
 
-daveFired = () => {
-  originalImage.style.opacity = "0"
-  return fireImage.style.opacity = "100%"
-};
+// daveFired = () => {
+//   originalImage.style.opacity = "0"
+//   return fireImage.style.opacity = "100%"
+// };
 
-feed.addEventListener(('click'), (taregt) => {
+feed.addEventListener(('click'), () => {
   daveFed()
   console.log(fedImage);
 });
 
-love.addEventListener(('click'), (taregt) => {
+love.addEventListener(('click'), () => {
   daveLoved()
   console.log(loveImage);
 });
+
 
 
 
