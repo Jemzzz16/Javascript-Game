15362699faeb2a592453191476e26c13170560dc
Jemma Holmes <jemmaholmes@Jemmas-MacBook-Pro.local>
2021-05-22 13:00:26 +0100

    Java added, buttons now click

--- main.js
+++ main.js
@@ -1,23 +1,53 @@
-const feed = document.querySelector('.feedhim');
-const love = document.querySelector('.lovehim');
-const fire = document.querySelector('.firehim');
+const feed = document.querySelector('#feedhim');
+const fedImage = document.querySelector('#davefed');
+
+const love = document.querySelector('#lovehim');
+const loveImage = document.querySelector('#daveloved');
+
+const fire = document.querySelector('#firehim');
+const fireImage = document.querySelector('#davefire');
+
+daveLoved = () => {
+  return loveImage
+};
+
+daveFired = () => {
+  return fireImage
+};
+
+daveFed = () => {
+  return fedImage
+};
+
+feed.addEventListener(('click'), (taregt) => {
+  daveLoved()
+  console.log(loveImage);
+});
 
 
-feed.addEventListener(('click'), (event) => {
+love.addEventListener(('click'), (taregt) => {
+  daveFired()
+  console.log(fireImage);
+});
 
-}
+fire.addEventListener(('click'), (taregt) => {
+  daveFed()
+  console.log(fedImage);
+});
 
 
 
-function changeImage() {
-  var image = document.getElementById('myImage');
-  if (image.src.match("colorbottel")) {
-      image.src = "waterbottel.gif";
-  }
-  else {
-      image.src = "colorbottel.gif";
-  }
-}
+
+
+// function changeImage() {
+//   var image = document.getElementById('myImage');
+//   if (image.src.match("colorbottel")) {
+//       image.src = "waterbottel.gif";
+//   }
+//   else {
+//       image.src = "colorbottel.gif";
+//   }
+// }
 
 // 
 
