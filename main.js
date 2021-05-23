ad3f8207ddcdaebf18ab47d1a6d663a4c744328b
Jemma Holmes <jemmaholmes@Jemmas-MacBook-Pro.local>
2021-05-23 22:22:07 +0100

    Background added, egg bug fixed

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const eggNormal = document.querySelector('#egg-normal')
 const eggSlightCrack = document.querySelector('#egg-slightly-cracked')
 const eggCracked = document.querySelector('#egg-cracked')
-const eggBtn = document.querySelector('#egg-button')
 
-const eggDiv = document.querySelector('.egg-images')
-const dragonDiv = document.querySelector('.dragon-images')
+
+const eggDiv = document.querySelector('.egg-div')
+const dragonDiv = document.querySelector('.dragon-div')
 
 
 const originalImage = document.querySelector('#dave-normal')
@@ -21,19 +21,30 @@ const fireImage = document.querySelector('#davefire');
 
 ////////////////////////////////////////////////////////////////////
 
-
-const firstCrack = () => {
-  return eggSlightCrack
-};          
-const secondCrack = () => {
-  return eggCracked
-};
-const finalCrack = () => {
-  eggDiv.style.style.zIndex = "1"
-  return dragonDiv.style.style.zIndex = "3"
-};
+// const eggBtn = document.querySelector('#egg-button')
+// for(i = 0; i < eggBtn.length; i++) {
+//   eggBtn[i].addEventListener(('click'), () => {
+//     firstCrack()
+//     console.log(firstCrack());
+//   })  
+// };
 
 ////////////////////////////////////////////////////////////////////
+// EGGS
+const firstCrack = () => {
+  eggNormal.style.zIndex = "1"
+  eggSlightCrack.style.opacity = "100%"
+  eggSlightCrack.style.zIndex = '3'
+};          
+const secondCrack = () => {
+  eggSlightCrack.style.zIndex = '1'
+  eggCracked.style.opacity = "100%"
+  eggCracked.style.zIndex = '3'
+};
+const finalCrack = () => {
+  eggDiv.style.opacity = "0"
+  return dragonDiv.style.opacity = "100%"
+};
 
 eggNormal.addEventListener(('click'), () => {
   firstCrack()
@@ -49,20 +60,26 @@ eggCracked.addEventListener(('click'), () => {
 });
 
 ////////////////////////////////////////////////////////////////////
-
+// DAVE MOOD
 daveFed = () => {
   originalImage.style.opacity = "0"
   return fedImage.style.opacity = "100%"
 };
-
 daveLoved = () => {
   originalImage.style.opacity = "0"
   return loveImage.style.opacity = "100%"
 };
 
+if (daveloved() = true )
+
+
 // daveFired = () => {
 //   originalImage.style.opacity = "0"
 //   return fireImage.style.opacity = "100%"
+// };
+// daveFlap = () => {
+//   originalImage.style.opacity = "0"
+//   return flapImage.style.opacity = "100"
 // };
 
 ////////////////////////////////////////////////////////////////////
@@ -77,29 +94,13 @@ love.addEventListener(('click'), () => {
   console.log(loveImage);
 });
 
-//for(i = 0; i < 5; i++) {
-  //   eggNormal.addEventListener(('click'), () => {
-  //     firstCrack = () => {
-  //       if(eggBtn = i)
-  //     }
-  //   })  
-  // };
-  // console.log(firstCrack());
+// setInterval(daveFlap(), 3*1000);
 
+// setTimeout(daveFired(), 5*1000);
 
-//setTimeout(daveFired(), 5*1000);
-
-// fire.addEventListener(('click'), (taregt) => {
+// fire.addEventListener(('click'), (event) => {
 //   daveFired()
 //   console.log(fireImage);
 // });
-
-// daveFlap = () => {
-//   originalImage.style.opacity = "0"
-//   return flapImage.style.opacity = "100"
-// };
-
-// setInterval(daveFlap(), 3*1000);
-
 
 // settimeout() - can be called for the fire image so after a certain amount of time this image will show 
